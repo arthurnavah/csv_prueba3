1903e421fb9b934eae0d34f20ea703e70d6a8589
César <c.caycho46@gmail.com>
2020-11-28 17:49:08 -0500

    primer error

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+)
+
 func main() {
+
+	data := `fish,blue,water
+dog,brown,house
+tiger,orange,jungle
+crocodrile,green,river`
+
+	lector := csv.NewReader(strings.NewReader(data))
+
+	contenido, err := lector.Read()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	archivo, err := os.OpenFile("databases.csv", os.O_RDWR, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer archivo.Close()
+	escritor := csv.NewWriter(archivo)
+
+	err = escritor.Write(contenido)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
